53a673397cc6eddaf839e61616109ee0ea11a6c8
周智伟 <zhouzhiwei@laihua.com>
2020-11-13 09:45:54 +0800

    Initial Commit

---
+++ VGCameraVideo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VGCameraVideo
+//
+//  Created by 周智伟 on 2020/11/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
